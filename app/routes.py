cc1dc75e1901b0f7c14ebcd6767145700a220441
Roman Verkhovsky <Roman081091@yandex.ru>
2024-07-28 19:20:14 +0400

    feat: templates - base, index, login, profile, register; fix: routes - add index, logout, profile, login, register

--- app/routes.py
+++ app/routes.py
@@ -13,3 +13,62 @@ bp = Blueprint('routes', __name__)
 @bp.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+
+@bp.route('/logout')
+def logout():
+    logout_user()
+
+    return redirect(url_for('routes.index'))
+
+
+@bp.route('/profile')
+@login_required
+def profile():
+    return render_template('profile.html', title='Profile', name=current_user.username)
+
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('routes.profile'))
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user is None or not user.check_password(form.password.data):
+            flash('Неверный адрес электронной почты или пароль!')
+            return redirect(url_for('routes.login'))
+
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('routes.profile')
+
+        return redirect(next_page)
+
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('routes.profile'))
+
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Поздравляем, теперь вы зарегистрировались в системе!')
+
+        return redirect(url_for('routes.login'))
+
+    return render_template('register.html', title='Register', form=form)
